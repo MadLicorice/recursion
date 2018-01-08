07f570bbfdf74d178dd095efb0b5ef57de7c5aba
MadLicorice <madlicorice@gmail.com>
2018-01-08 18:47:55 -0500

    Finish getElementsByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let result = [];
+
+  let search = function(element) {
+  	//let list = element.classList;
+
+  	if(element.classList && element.classList.contains(className)) {
+  		result.push(element);
+  	}
+
+  	for (let i = 0; i < element.childNodes.length; i+=1) {
+  		search(element.childNodes[i]);
+  	}
+
+  }
+
+  search(document.body);
+  return result;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,62 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = '';
+
+  if (Array.isArray(obj)) {
+    result += '[';
+  } else if (typeof obj === 'object' && obj !== null) {
+    result += '{';
+  }
+
+  if (typeof obj === 'object' && obj !== null) {
+
+    _.each(obj, function(item, indexOrKey) {
+      if (typeof item !== 'function' && typeof item !== 'undefined') {
+        if (typeof indexOrKey === 'string') {
+          result += '"'+indexOrKey+'":';
+        }
+        
+        if (item === null) {
+          result += 'null';
+        } else if (typeof item === 'string') {
+          result += '"'+item+'"';
+        } else if (typeof item === 'object' && item !== null) {
+          result += stringifyJSON(item);
+        } else if (typeof item !== 'function' && typeof item !== 'undefined') {
+          result += item.toString();
+        } 
+
+        if (Array.isArray(obj)) {
+          if (indexOrKey < obj.length - 1) {
+            result += ',';
+          }
+        } else if (typeof obj === 'object' && obj !== null) {
+          let keys = Object.keys(obj);
+          if (indexOrKey !== keys[keys.length - 1]) {
+            result += ',';
+          }
+        }
+      }
+    });
+
+  } else {
+
+    if (obj === null) {
+      result += 'null';
+    } else if (typeof obj === 'string') {
+      result += '"'+obj+'"';
+    } else if (typeof obj !== 'function' && typeof obj !== 'undefined') {
+      result += obj.toString();
+    }
+  }
+
+  if (Array.isArray(obj)) {
+    result += ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    result += '}';
+  }
+
+  return result;
+
 };
